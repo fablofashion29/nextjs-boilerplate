c634f81132ab574e6933c268a22a31b81e8ccc6b
fablofashion29 <fablofashion29@gmail.com>
2025-10-22 17:58:36 -0400

    Remove a file

--- app/api/contact/route-old.ts
+++
@@ -1,9 +0,0 @@
-import resend from "resend"
-const email = "your-email@example.com" // Replace with your actual email
-
-await resend.emails.send({
-  from: "onboarding@resend.dev", // Resend's test email
-  to: email,
-  subject: `Contact Form: ${name}`,
-  text: `You have a new contact form submission from ${name} with email ${email}.`,
-})
